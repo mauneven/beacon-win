de1e4dd9b1e7e92241c93ca7501f5cd5ee91c003
mauneven <mauneven@gmail.com>
2025-02-15 21:47:59 -0500

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.UI.Xaml;
+using System;
+
+namespace beacon
+{
+    public partial class App : Application
+    {
+        private Window? m_window;
+        public static MainWindow? CurrentMainWindow { get; private set; }
+
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+        {
+            m_window = new MainWindow();
+            CurrentMainWindow = m_window as MainWindow;
+            m_window.Activate();
+        }
+    }
+}
---
+++ BeaconApp/Pages/Settings/Settings.xaml.cs
@@ -0,0 +1,34 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.ApplicationModel.Core;
+
+namespace beacon.BeaconApp.Pages.Settings
+{
+    public sealed partial class Settings : Page
+    {
+        public Settings()
+        {
+            Windows.ApplicationModel.Resources.Core.ResourceManager.Current.DefaultContext.Reset();
+            this.InitializeComponent();
+        }
+
+        private void ChangeLanguage_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is not Button button) return;
+            Windows.Globalization.ApplicationLanguages.PrimaryLanguageOverride = button.Tag as string;
+            Windows.ApplicationModel.Resources.Core.ResourceManager.Current.DefaultContext.Reset();
+
+            if (App.CurrentMainWindow is not null)
+            {
+                App.CurrentMainWindow.MainFrame.Navigate(typeof(beacon.BeaconApp.Pages.Settings.Settings));
+                Microsoft.Windows.AppLifecycle.AppInstance.Restart("");
+            }
+        }
+
+        private void GoToMain_Click(object sender, RoutedEventArgs e)
+        {
+            // Navigate back to the home page using its fully-qualified type.
+            this.Frame.Navigate(typeof(beacon.BeaconApp.Pages.Home.Home));
+        }
+    }
+}
